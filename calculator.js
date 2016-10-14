22bcd9f13b74de7ed6536df7b9129100f2a188f1
Daniel Alderman <daniel.alderman77@gmail.com>
2016-10-14 20:47:29 +0100

    Added TODOs. Imported script into html

--- calculator.js
+++ calculator.js
@@ -1,3 +1,14 @@
 /**
  * Created by danielalderman on 14/10/2016.
  */
+
+<!--TODO addition-->
+function addition() {
+    return '+';
+}
+
+<!--TODO subtraction-->
+<!--TODO multiplication-->
+<!--TODO division-->
+<!--TODO equals-->
+<!--TODO clear-->
